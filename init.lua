1f1ef37647e1986ffff5c9c272cbb16131625a29
1garo <alevardai427@gmail.com>
2025-12-21 15:35:04 -0300

    some updates

--- init.lua
+++ init.lua
@@ -531,15 +531,13 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    }
-  end,
-}
+for server, config in pairs(servers) do
+  vim.lsp.config(server, {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = config,
+  })
+end
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
